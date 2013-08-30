fbec9a96e5e5594c474424d2af4968604685298e
tekkoc <tekkoc@gmail.com>
2013-08-30 23:19:53 +0900

    slate

--- .slate.js
+++ .slate.js
@@ -1,155 +1,124 @@
-//**********************************
-// 参考サイト
-//
-// [コピペ元](http://mint.hateblo.jp/entry/2013/01/29/003541)
-// [↑の方のメモ](https://gist.github.com/jewel12/4656239)
-// [紹介記事](http://mint.hateblo.jp/entry/2012/12/29/152742)
-// [JSでの変更法(英語)](https://github.com/jigish/slate/wiki/JavaScript-Configs)
-//
-// 
-//**********************************
-// bind m:shift;cmd hint ASDFGHJKLQWERTYUIOPCVBN
-//**********************************
-// ヘルパー
-//**********************************
+// TODO FPSのように、123などで指定アプリケーションをフォーカスする？
 
-var resize = function (width, height, anchor) {
-    if (typeof anchor === 'undefined') anchor = 'top-left';
-
-    return slate.operation('resize', {
-        'width' : width,
-           'height' : height,
-           'anchor' : anchor
+// .slate.js
+var util = {
+  // alt + ..
+  key: function(k, mod) {
+    return k + ':alt' + (mod ? ',' + mod : '');
+  },
+  focusWindow: function(f) {
+    var hit = false;
+    slate.eachApp(function(app) {
+      if (hit) return;
+      app.eachWindow(function(win) {
+        if (hit) return;
+        if (f(win)) {
+          win.focus();
+          hit = true;
+        }
+      });
     });
+  },
+  nextScreen: function(screen) {
+    return slate.screenForRef(String( (screen.id()+1)%slate.screenCount() ));
+  }
 };
+ 
+// return     .. macvim
+slate.bind(util.key('return'), slate.operation('focus', {
+  'app' : "MacVim"
+}));
 
-var nudge = function (x, y) {
-    return slate.operation('nudge', {
-        'x' : x,
-           'y' : y
-    });
-};
-
-var focus = function (app_name) {
-    return slate.operation('focus', {'app' : app_name});
-};
-
-var chain = function(actions) {
-    return slate.operation('chain', {'operations' : actions});
-};
-
-var half_push = function(direction) {
-    return slate.operation('push', {
-        'direction' : direction,
-           'style' : 'bar-resize:screenSizeX/2'
-    });
-};
-
-var half_top = function(direction) {
-    return slate.operation('corner', {
-        'direction' : 'top-'+direction,
-           'width' : 'screenSizeX/2',
-           'height' : 'screenSizeY/2'
-    });
-};
-
-var half_bottom = function(direction) {
-    return slate.operation('corner', {
-        'direction' : 'bottom-'+direction,
-           'width' : 'screenSizeX/2',
-           'height' : 'screenSizeY/2'
-    });
-};
-
-//**********************************
-// 基本設定
-//**********************************
-
-S.configAll({
-    defaultToCurrentScreen:true,
-    nudgePercentOf:'screenSize',
-    resizePercentOf:'screenSize'
-});
-
-//**********************************
-// キーバインドの設定
-//**********************************
-
-var key_binds = {};
-
-// Reload
-key_binds['r:ctrl,shift,alt'] = slate.operation('relaunch');
-
-// 全画面化
-var pre_fullscreen = 'alt';
-key_binds['return:'+pre_fullscreen] = slate.operation('corner', {
-    'direction' : 'top-left',
-    'width' : 'screenSizeX',
-    'height' : 'screenSizeY'
-});
-
-var pre_half = 'alt';
-key_binds['left:'+pre_half] = slate.operation("push", {
-  "direction" : "left",
-  "style" : "bar-resize:screenSizeX/2"
-});
-key_binds['right:'+pre_half] = slate.operation("push", {
-  "direction" : "right",
-  "style" : "bar-resize:screenSizeX/2"
-});
-key_binds['up:'+pre_half] = slate.operation("push", {
-  "direction" : "up",
-  "style" : "bar-resize:screenSizeY/2"
-});
-key_binds['down:'+pre_half] = slate.operation("push", {
-  "direction" : "down",
-  "style" : "bar-resize:screenSizeY/2"
-});
-
-var pre_quarter = 'alt,shift';
-key_binds['left:'+pre_quarter] = slate.operation("corner", {
-    "direction" : "top-left",
-    'width' : 'screenSizeX/2',
-    'height' : 'screenSizeY/2'
-});
-key_binds['right:'+pre_quarter] = slate.operation("corner", {
-    "direction" : "bottom-right",
-    'width' : 'screenSizeX/2',
-    'height' : 'screenSizeY/2'
-});
-key_binds['up:'+pre_quarter] = slate.operation("corner", {
-    "direction" : "top-right",
-    'width' : 'screenSizeX/2',
-    'height' : 'screenSizeY/2'
-});
-key_binds['down:'+pre_quarter] = slate.operation("corner", {
-    "direction" : "bottom-left",
-    'width' : 'screenSizeX/2',
-    'height' : 'screenSizeY/2'
-});
-
-// Undo
-// key_binds['`:alt'] = slate.operation('undo');
-
-// Forcus
-key_binds['j:alt'] = focus('Firefox');
-key_binds['k:alt'] = focus('MacVim');
-key_binds['l:alt'] = focus('ターミナル');
-key_binds[';:alt'] = focus('Skype');
-
-key_binds['space:alt'] = slate.operation("hint", {
+// space      .. hint
+slate.bind(util.key('space'), slate.operation("hint", {
   "characters" : "ASDFGHJKLQWERTYUIOPCVBN"
+}));
+ 
+// r+ctrl     .. Slateの再起動
+slate.bind(util.key('r', 'ctrl'), function(win) {
+  slate.operation('relaunch').run();
+});
+ 
+// hjkl       .. その方向へフォーカス移動
+slate.bind(util.key('h'), slate.operation('focus', { direction: 'left' }));
+slate.bind(util.key('j'), slate.operation('focus', { direction: 'down' }));
+slate.bind(util.key('k'), slate.operation('focus', { direction: 'up' }));
+slate.bind(util.key('l'), slate.operation('focus', { direction: 'right' }));
+ 
+// i          .. 下に隠れているウィンドウをフォーカス
+slate.bind(util.key('i'), slate.operation('focus', { direction: 'behind' }));
+ 
+// o          .. スクリーン間でフォーカスを移動
+slate.bind(util.key('o'), function(win) {
+  var next = util.nextScreen(slate.screen());
+ 
+  util.focusWindow(function(win) {
+    return win.screen().id() == next.id();
+  });
+});
+ 
+// o+shift    .. 次のスクリーンへ飛ばす
+slate.bind(util.key('o', 'shift'), function(win) {
+  if (!win) return;
+  var next = util.nextScreen(win.screen());
+ 
+  win.move(next.visibleRect());
+});
+ 
+// j+shift    .. 4隅に飛ばす
+var corners = slate.bind(util.key('j', 'shift'), slate.operation('chain', {
+  operations: _.map(['top-right', 'bottom-right', 'bottom-left', 'top-left'], function(d) {
+    return slate.operation('corner', {
+      direction: d,
+      width: 'screenSizeX/2',
+      height: 'screenSizeY/2'
+    });
+  })
+}));
+ 
+// k+shift    .. 左右に飛ばす
+slate.bind(util.key('k', 'shift'), slate.operation('chain', {
+  operations: _.map(['left', 'right'], function(d) {
+    return slate.operation('push', {
+      direction: d,
+      style: 'bar-resize:screenSizeX/2'
+    });
+  })
+}));
+ 
+// h+shift   .. ウィンドウが左にあるなら縮小, 右にあるなら拡大
+slate.bind(util.key('h', 'shift'), function(win) {
+  if (!win) return;
+  var rect = win.rect();
+  var bounds = win.screen().visibleRect();
+  if (bounds.x + bounds.width - 30 < rect.x + rect.width) {
+    rect.x -= bounds.width * 0.05;
+    rect.width = bounds.x + bounds.width - rect.x;
+  } else {
+    rect.width -= bounds.width * 0.05;
+  }
+  win.doOperation('move', rect);
+});
+ 
+// l+shift   .. ウィンドウが右にあるなら縮小, 左にあるなら拡大
+slate.bind(util.key('l', 'shift'), function(win) {
+  if (!win) return;
+  var rect = win.rect();
+  var bounds = win.screen().visibleRect();
+  if (rect.x < bounds.x + 30) {
+    rect.x = bounds.x;
+    rect.width += bounds.width * 0.05;
+  } else {
+    rect.x += bounds.width * 0.05;
+    rect.width -= bounds.width * 0.05;
+  }
+  win.doOperation('move', rect);
+});
+ 
+// m          .. 最大化
+slate.bind(util.key('m'), function(win) {
+  if (!win) return;
+  var bounds = win.screen().visibleRect();
+  win.doOperation('move', bounds);
 });
 
-S.bindAll(key_binds);
-
-// // フォーカスしているアプリを終了
-// S.bind('c:alt,shift', function(win) {
-//     var pid = win.app().pid();
-//     slate.log('Kill process ' + pid);
-//     win.doOperation(
-//         slate.operation('shell', {
-//             'command' : '/bin/kill '+ pid
-//         })
-//         );
-// });
